b37a2aa093a2716307c96d6a4e591b6ab7605322
PauliusVait <paulius.vaitkevicius@vinted.com>
2025-03-25 10:59:58 +0200

    Ch. 3 added sys.argv to main

--- main.py
+++ main.py
@@ -17,7 +17,10 @@ def print_report(book_path,num_words,sorted_chars):
 
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     char_counts_dict = count_letters(text)
